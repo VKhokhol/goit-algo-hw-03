a1796ee3c88cb1d55bde066d5b97a1535a7c0c52
VKhokhol <v.khokhol88@gmail.com>
2025-08-23 04:20:31 +0300

    adjustment

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,16 +1,22 @@
+#Імпортування модуля random
 import random
+#Створення функції з трьома параметрами
 def get_numbers_ticket(min, max, quantity):
 
+#Створення пустої множини
     numbers_ticket=set()
+#Перевірка умови про діапазони значень min, max
     if min < 1 or max > 1000:
         return numbers_ticket
-    
+
+#Створення циклу, в якому перевіряється довжина множини і якщо вона менше quantity, 
+#то генерується випадкове число і додається до множини
     while len(numbers_ticket)< quantity:
         number = random.randint(min, max)
         numbers_ticket.add(number)
-
+#Перетворенням множини в список, сортування його та повернення із функції
     numbers_ticket_set_to_list = list(numbers_ticket)
     sorted_numbers_ticket = sorted(numbers_ticket_set_to_list)
     return sorted_numbers_ticket
-
+#Перевірка результату роботи
 print(get_numbers_ticket(1, 1000, 5))
