c513224f5f67c787ec4b9bf5ecd0e67e311dab76
VKhokhol <v.khokhol88@gmail.com>
2025-08-23 04:23:45 +0300

    adjustment

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,9 +1,16 @@
+#Імпортування модуля datetime
 import datetime
+#Створення змінної date яку пропонується ввести користувачу
 date = input('Введіть дату в форматі YYYY-MM-DD: ')
+#Створення функції з параметром date
 def date_diff(date):
+#Визначення сьогоднішньої дати
     today = datetime.datetime.now().date()
+#Перетворення отриманої дати від користувача в об'єкт datetime
     given_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+#Обрахунок різниці між двома датами за модулем
     difference = abs((today - given_date)).days
     return difference
+#Перевірка результатів
 difference = date_diff(date)
 print(f'Різниця між сьогоднішьою датою та заданою: {difference} днів')
