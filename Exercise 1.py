5ec062fc934745ea9313acd88b13f4f0fb29c954
VKhokhol <v.khokhol88@gmail.com>
2025-08-23 03:38:18 +0300

    first adjustment

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,7 +1,9 @@
 import datetime
+date = input('Введіть дату в форматі YYYY-MM-DD: ')
 def date_diff(date):
     today = datetime.datetime.now().date()
     given_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
     difference = abs((today - given_date)).days
     return difference
-print(date_diff('2025-08-25'))
+difference = date_diff(date)
+print(f'Різниця між сьогоднішьою датою та заданою: {difference} днів')
---
+++ Exercise 2.py
@@ -0,0 +1,3 @@
+import random
+def get_numbers_ticket(min, max, quantity):
+    
