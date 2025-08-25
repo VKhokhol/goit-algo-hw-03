74dd61564028f4e20f8bb36c9e03b35829fb023b
VKhokhol <v.khokhol88@gmail.com>
2025-08-25 04:21:48 +0300

    adjustment

--- Exercise 1.py
+++ Exercise 1.py
@@ -3,14 +3,22 @@ import datetime
 #Створення змінної date яку пропонується ввести користувачу
 date = input('Введіть дату в форматі YYYY-MM-DD: ')
 #Створення функції з параметром date
-def date_diff(date):
-#Визначення сьогоднішньої дати
-    today = datetime.datetime.now().date()
+def get_days_from_today(date):
+#Створення блоку try, в якому виконується спроба обрахувати різницю
+    try:
+ #Визначення сьогоднішньої дати       
+        today = datetime.datetime.now().date()
 #Перетворення отриманої дати від користувача в об'єкт datetime
-    given_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+        given_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
 #Обрахунок різниці між двома датами за модулем
-    difference = abs((today - given_date)).days
-    return difference
+        difference = (given_date-today).days
+        return difference
+#Якщо користувач вводить дату в неправильному форматі - вилазить помилка ValyeError,
+#  яка перехоплюється та виводиться повідомлення для користувача
+    except ValueError:
+        print('Неправильний формат дати. Задайте дату у вигляді YYYY-MM-DD')
+        return None
 #Перевірка результатів
-difference = date_diff(date)
-print(f'Різниця між сьогоднішьою датою та заданою: {difference} днів')
+difference = get_days_from_today(date)
+if difference is not None:
+    print(f'Різниця між сьогоднішьою датою та заданою: {difference} днів')
